ee6680326854d370c0e9d5f00a17e1a46f5105b7
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-07-17 14:59:05 -0700

    minor update

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -3,8 +3,8 @@ type module >& /dev/null || source /mnt/software/Modules/current/init/bash
 module load git/2.8.3
 module load gcc/4.9.2
 module load cmake/3.7.2
-module load ccache_base/3.2.3
-module load zlib/1.2.5
+module load ccache/3.2.3
+module load zlib/1.2.8
 module load ninja/1.7.1
 module load boost/1.60
 unset PKG_CONFIG_LIST
@@ -19,7 +19,7 @@ if [ ! -d build ] ; then mkdir build ; fi
 echo "## Build binaries"
 export CXXFLAGS="-fPIC -static-libstdc++"
 ( cd build && rm -rf * )
-( cd build && cmake -DZLIB_INCLUDE_DIR=/mnt/software/z/zlib/1.2.5/include -DZLIB_LIBRARY=/mnt/software/z/zlib/1.2.5/lib/libz.so -GNinja .. )
+( cd build && cmake -DZLIB_INCLUDE_DIR=`pkg-config --cflags-only-I zlib|sed -e 's/-I//'` -DZLIB_LIBRARY=`pkg-config --libs-only-L zlib|sed -e 's/-L//'`/libz.so -GNinja .. )
 ( cd build && ninja htslibSrc )
 ( cd build && ninja )
 
