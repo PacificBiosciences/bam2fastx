97e6c55e7844e5c26425f30c1aed4fe12fade7db
Derek Barnett <dbarnett@pacificbiosciences.com>
2017-07-29 00:51:57 -0700

    updating to use gnu module htslib, rather than bundled in pbbam

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -7,6 +7,7 @@ module load ccache/3.2.3
 module load zlib/1.2.8
 module load ninja/1.7.1
 module load boost/1.60
+module load htslib/1.3.1
 unset PKG_CONFIG_LIST
 export CCACHE_BASEDIR=$PWD
 export VERBOSE=1
