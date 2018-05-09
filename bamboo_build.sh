ea208af08802b082ccc6d41b0442971a6ae5971d
David Seifert <dseifert@pacificbiosciences.com>
2018-05-09 12:35:32 +0200

    Use Meson in CI

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,32 +1,58 @@
-#!/bin/bash -evx
-type module >& /dev/null || source /mnt/software/Modules/current/init/bash
-module load git
+#!/usr/bin/env bash
+set -vex
+
+################
+# DEPENDENCIES #
+################
+
+## Load modules
+type module >& /dev/null || . /mnt/software/Modules/current/init/bash
+
+module purge
+
 module load gcc
-module load cmake
 module load ccache
-module load zlib
+
+module load meson
 module load ninja
+
 module load boost
-module load htslib
-unset PKG_CONFIG_LIST
-if [[ $USER == "bamboo" ]]; then
-  export CCACHE_DIR=/mnt/secondary/Share/tmp/bamboo.mobs.ccachedir
+module load zlib
+
+module load cram
+
+case "${bamboo_planRepository_branchName}" in
+  master)
+    module load pbbam/master
+    module load pbcopper/master
+    ;;
+  *)
+    module load pbbam/develop
+    module load pbcopper/develop
+    ;;
+esac
+
+
+BOOST_ROOT="${BOOST_ROOT%/include}"
+# unset these variables to have meson discover all
+# boost-dependent variables from BOOST_ROOT alone
+unset BOOST_INCLUDEDIR
+unset BOOST_LIBRARYDIR
+
+export CC="ccache gcc"
+export CXX="ccache g++"
+export CCACHE_BASEDIR="${PWD}"
+
+if [[ $USER == bamboo ]]; then
+  export CCACHE_DIR=/mnt/secondary/Share/tmp/bamboo.${bamboo_shortPlanKey}.ccachedir
   export CCACHE_TEMPDIR=/scratch/bamboo.ccache_tempdir
 fi
-export CCACHE_COMPILERCHECK='%compiler% -dumpversion'
-export CCACHE_BASEDIR=$PWD
-export VERBOSE=1
 
-echo "# BUILD AND TEST"
+# call the main build+test scripts
+export CURRENT_BUILD_DIR="build"
+export ENABLED_TESTS="true"
 
-echo "## Enter build directory "
-if [ ! -d build ] ; then mkdir build ; fi
+export LDFLAGS="-static-libstdc++ -static-libgcc"
 
-echo "## Build binaries"
-export CXXFLAGS="-fPIC -static-libstdc++"
-( cd build && rm -rf * )
-( cd build && cmake -GNinja .. )
-( cd build && sed -i -e 's@/-I/mnt/software@/ -I/mnt/software@g' build.ninja && ninja )
-
-echo "## Test cram tests"
-( cd build && ninja check)
+bash scripts/ci/build.sh
+bash scripts/ci/test.sh
